1faa5f5bcf327834bcbe143ea8d9bc8b3d7d8fe5
unknown <ricky.fourlight@gmail.com>
2026-06-04 15:29:56 +0700

    upload baru ke main

--- index.php
+++ index.php
@@ -9,13 +9,15 @@ include 'header.php';
     <div class="body-inner">
 
 
-    
+
 
         <!-- Inspiro Slider -->
-        <div id="slider" class="inspiro-slider slider-halfscreen dots-dark arrows-dark dots-creative" data-fade="true" data-height-xs="360">
+        <div id="slider" class="inspiro-slider slider-halfscreen dots-dark arrows-dark dots-creative" data-fade="true"
+            data-height-xs="360">
 
             <!-- Slide 1 -->
-            <div class="slide background-image section-overlay-dark" style="background-image:url('images/organisasi.jpeg');">
+            <div class="slide background-image section-overlay-dark"
+                style="background-image:url('images/organisasi.jpeg');">
                 <div class="container">
                     <div class="slide-captions text-end">
                         <!-- Captions -->
@@ -39,17 +41,21 @@ include 'header.php';
             <div class="row">
                 <div class="col-lg-6 text-end" style="background-color: #111828">
                     <h2>VISI</h2>
-                    <span class="lead">Menciptakan nilai bersama untuk meningkatkan kesejahteraan masyarakat Indonesia</span>
+                    <span class="lead">Menciptakan nilai bersama untuk meningkatkan kesejahteraan masyarakat
+                        Indonesia</span>
                 </div>
 
                 <div class="col-lg-6 text-start" style="background-color: #111828">
                     <h2>MISI</h2>
                     <span class="lead">
                         <ul style="text-align: left;">
-                            <li style="color: white; margin-bottom: 10px;">Menyediakan solusi pembiayaan yang inovatif dan terpercaya.</li> 
-                            <li style="color: white; margin-bottom: 10px;">Memberikan pelayanan terbaik kepada sahabat Adira.</li> 
-                            <li style="color: white; margin-bottom: 10px;">Mendukung pertumbuhan ekonomi sahabat Adira.</li> 
-                            
+                            <li style="color: white; margin-bottom: 10px;">Menyediakan solusi pembiayaan yang inovatif
+                                dan terpercaya.</li>
+                            <li style="color: white; margin-bottom: 10px;">Memberikan pelayanan terbaik kepada sahabat
+                                Adira.</li>
+                            <li style="color: white; margin-bottom: 10px;">Mendukung pertumbuhan ekonomi sahabat Adira.
+                            </li>
+
                         </ul>
                     </span>
                 </div>
@@ -57,35 +63,47 @@ include 'header.php';
         </section>
         <!-- end: MISSION & VISSION -->
 
-             <!-- HISTORY -->
+        <!-- HISTORY -->
         <section class="background-grey p-b-60">
             <div class="container">
                 <div class="heading-text heading-section text-center m-b-40">
                     <h2>Struktur Organisasi</h2>
-                    
+
                 </div>
                 <div class="row">
                     <div class="col-lg-12">
                         <div class="m-b-40">
                             <div class="center">
-                     
-                       
 
 
-                          <span class="lead" style="text-align: center;">Struktur organisasi Adira Finance dipimpin oleh Direktur Utama yang bertanggung jawab atas arah dan strategi perusahaan. Dalam menjalankan kegiatan perusahaan, Direktur Utama didukung oleh beberapa direktur yang membawahi bidang keuangan, operasional, pemasaran (marketing), serta manajemen risiko dan sumber daya manusia.
-                          <br>Di bawah jajaran direksi terdapat kepala divisi dan kepala departemen yang bertugas mengelola program kerja sesuai bidang masing-masing. Selanjutnya terdapat manajer cabang yang mengawasi operasional di berbagai wilayah Indonesia agar pelayanan kepada pelanggan berjalan dengan baik.
-                          <br>Dalam pelaksanaan kegiatan sehari-hari, operasional perusahaan didukung oleh supervisor, customer service, dan staf operasional yang berperan memberikan pelayanan pembiayaan serta membantu kebutuhan nasabah secara langsung.Struktur organisasi ini membantu Adira Finance menjalankan kegiatan usaha secara efektif, profesional, dan terkoordinasi dalam memberikan layanan pembiayaan kepada masyarakat Indonesia. </span>
+
+
+                                <span class="lead" style="text-align: center;">Struktur organisasi Adira Finance
+                                    dipimpin oleh Direktur Utama yang bertanggung jawab atas arah dan strategi
+                                    perusahaan. Dalam menjalankan kegiatan perusahaan, Direktur Utama didukung oleh
+                                    beberapa direktur yang membawahi bidang keuangan, operasional, pemasaran
+                                    (marketing), serta manajemen risiko dan sumber daya manusia.
+                                    <br>Di bawah jajaran direksi terdapat kepala divisi dan kepala departemen yang
+                                    bertugas mengelola program kerja sesuai bidang masing-masing. Selanjutnya terdapat
+                                    manajer cabang yang mengawasi operasional di berbagai wilayah Indonesia agar
+                                    pelayanan kepada pelanggan berjalan dengan baik.
+                                    <br>Dalam pelaksanaan kegiatan sehari-hari, operasional perusahaan didukung oleh
+                                    supervisor, customer service, dan staf operasional yang berperan memberikan
+                                    pelayanan pembiayaan serta membantu kebutuhan nasabah secara langsung.Struktur
+                                    organisasi ini membantu Adira Finance menjalankan kegiatan usaha secara efektif,
+                                    profesional, dan terkoordinasi dalam memberikan layanan pembiayaan kepada masyarakat
+                                    Indonesia. </span>
+                            </div>
                         </div>
-</div>
 
                     </div>
-                   
+
                 </div>
             </div>
         </section>
         <!-- end: HISTORY -->
 
-                <!-- WHAT WE DO -->
+        <!-- WHAT WE DO -->
         <section>
             <div class="container">
 
@@ -113,21 +131,25 @@ include 'header.php';
                         </div>
                     </div>
 
-            
+
                 </div>
             </div>
         </section>
         <!-- END WHAT WE DO -->
 
 
-        
+
 
         <!-- PARALLAX -->
-        <section class="text-center text-light p-t-150 p-b-150 section-overlay-dark" data-bg-parallax="images/berita.jpeg">
+        <section class="text-center text-light p-t-150 p-b-150 section-overlay-dark"
+            data-bg-parallax="images/berita.jpeg">
             <div class="container">
                 <h4>BERITA</h4>
-                <h2>Adira Finance Menggencarkan Program Recovery: Ulurkan Tangan Sahabat untuk Nasabah yang Sedang Berjuang</h2>
-                <p class="lead"> PT Adira Dinamika Multi Finance Tbk (Adira Finance) kembali menunjukkan komitmennya dalam mendukung masyarakat melalui pelaksanaan Program Recovery yang mulai dijalankan secara nasional di seluruh cabang Adira Finance di Indonesia...</p>
+                <h2>Adira Finance Menggencarkan Program Recovery: Ulurkan Tangan Sahabat untuk Nasabah yang Sedang
+                    Berjuang</h2>
+                <p class="lead"> PT Adira Dinamika Multi Finance Tbk (Adira Finance) kembali menunjukkan komitmennya
+                    dalam mendukung masyarakat melalui pelaksanaan Program Recovery yang mulai dijalankan secara
+                    nasional di seluruh cabang Adira Finance di Indonesia...</p>
 
                 <a href="blog.php" class="btn btn-outline btn-light"><span>Baca Selengkapnya</span></a>
             </div>
@@ -141,7 +163,7 @@ include 'header.php';
         <?php include 'footer.php'; ?>
         <!-- end: Footer -->
 
-       
+
 
     </div>
     <!-- end: Body Inner -->
